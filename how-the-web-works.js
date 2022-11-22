0b450548899b51c4fd6ff7c09122d4666858f705
Kevin Trinh <kevintrinh@Computer.local>
2022-11-22 11:38:42 -0800

    add file

---
+++ how-the-web-works.js
@@ -0,0 +1,26 @@
+// What is HTTP?
+// HTTP is Hypertext Transfer Protocol, and it is how browsers and servers communicate.
+
+// What is a URL?
+// URL is Uniform Resource Locator, and it is an address for an internet resource.
+
+// What is DNS?
+// DNS is Domain Name System, and it converts URLs into IP addresses.
+
+// What is a query string?
+// A query string essentially acts as a "search" for specific pages in a URL.
+
+// What are two HTTP verbs and how are they different?
+// Two HTTP verbs are GET and POST. GET are requests without side effects, while POST are requests with side effects.
+
+// What is an HTTP request?
+// An HTTP request is a request from a client to a server.
+
+// What is an HTTP response?
+// An HTTP response is a response from a server to a client.
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// An HTTP header provides additional information about the request or the response. Some examples are Host, Accept, Content-Type, and Last-Modified.
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// The browser converts the URL into an IP address using DNS; the browser makes a request to that IP address; the server responds; the browser makes a DOM from that HTML, and finds other resources needed; the browser makes HTTP requests for those resources and receives responses from the server.
